0689c310fa7c9cc06684465e3b3297b96c87375c
Augustin Viard <augustin.viard.etu@univ-lille.fr>
2026-08-06 16:22:50 +0200

    kanban desktop css done

--- components/KanbanColumn.tsx
+++ components/KanbanColumn.tsx
@@ -17,8 +17,11 @@ export function KanbanColumn({
     projects?: Record<string, string>
 }) {
     return (
-        <section className="flex flex-col w-full gap-4">
-            <h3>{title}</h3>
+        <section className="flex flex-col w-full gap-4 bg-white px-6 py-10 rounded-[10px]">
+            <div className="flex items-center gap-2 mb-6">
+                <h3 className="text-lg font-semibold">{title}</h3>
+                <span className="text-sm text-(--neutral-grey) bg-(--form-grey) px-4 py-1 rounded-full">{tasks.length}</span>
+            </div>
             {tasks.map((task) => (
                 <TaskCard key={task.id} task={task} members={members} canManageTasks={canManageTasks} currentUserId={currentUserId} projects={projects} />
             ))}
