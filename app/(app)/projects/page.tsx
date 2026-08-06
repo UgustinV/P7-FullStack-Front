de0cc6c49cdbb396aae610928fc12572f2bc28f4
Augustin Viard <augustin.viard.etu@univ-lille.fr>
2026-08-06 17:49:30 +0200

    projects page desktop css done

--- app/(app)/projects/page.tsx
+++ app/(app)/projects/page.tsx
@@ -1,18 +1,24 @@
-import { getProjects } from '@/app/lib/dal'
+import { getProjects, getProjectTasks } from '@/app/lib/dal'
 import { ProjectCard } from '@/components/ProjectCard'
 import { CreateProjectModal } from '@/components/CreateProjectModal'
 
 export default async function Projects() {
     const projects = await getProjects()
+    const tasks = await Promise.all(projects.map(project => getProjectTasks(project.id)))
     return (
-        <div>
-            <div className="flex items-center justify-between">
-                <h1>Projets</h1>
-                <CreateProjectModal />
+        <div className="mx-25">
+            <div className='flex justify-between items-center mt-23 mb-15'>
+                <div className='flex flex-col gap-3.5'>
+                    <h1 className='text-2xl font-semibold'>Mes projets</h1>
+                    <h2>Gérer mes projets</h2>
+                </div>
+                <div>
+                    <CreateProjectModal />
+                </div>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
                 {projects.map((project) => (
-                    <ProjectCard key={project.id} project={project} />
+                    <ProjectCard key={project.id} project={project} tasks={tasks[projects.indexOf(project)]} />
                 ))}
             </div>
         </div>
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,24 +1,45 @@
 import Link from 'next/link'
 import { Project } from '@/app/lib/definitions'
+import { getInitials } from '@/app/lib/utils'
+import Image from 'next/image'
 
-export function ProjectCard({ project }: { project: Project }) {
+export function ProjectCard({ project, tasks }: { project: Project, tasks?: Project['tasks'] }) {
     return (
         <Link
             href={`/projects/${project.id}`}
-            className="flex flex-col gap-2 rounded-[10px] border border-(--form-grey) p-4 hover:shadow-md transition-shadow"
+            className="flex flex-col gap-2 rounded-[10px] border border-(--form-grey) text-(--neutral-grey) p-4 bg-white"
         >
-            <h3 className="font-semibold">{project.name}</h3>
-            <p className="text-sm text-(--neutral-grey)">{project.description}</p>
-            <div className="flex items-center justify-between text-xs text-(--neutral-grey)">
-                <span>Propriétaire : {project.owner.name}</span>
+            <h3 className="font-semibold text-lg text-black">{project.name}</h3>
+            <p className="text-sm">{project.description}</p>
+            <div className="my-14">
+                <div className="flex items-center justify-between text-xs">
+                    <span>Progression</span>
+                    <span className='text-black'>{tasks ? Math.round((tasks.filter(task => task.status === 'DONE')).length / tasks.length * 100) : 100}%</span>
+                </div>
+                <progress className="task-progress w-full h-1.75 rounded-full" value={tasks ? (tasks.filter(task => task.status === 'DONE')).length : 1} max={tasks ? tasks.length : 1}></progress>
+                <span className='text-[10px]'>{tasks ? (tasks.filter(task => task.status === 'DONE')).length : 1}/{tasks ? tasks.length : 1} tâche{tasks && (tasks.filter(task => task.status === 'DONE')).length > 1 ? 's' : ''} terminée{tasks && (tasks.filter(task => task.status === 'DONE')).length > 1 ? 's' : ''}</span>
+            </div>
+            <div className="flex items-center justify-left gap-2">
+                <Image src="/team-icon.svg" alt="Équipe" width={11} height={11} />
+                <span className='text-[10px]'>Équipe ({project.members.length + 1})</span>
+            </div>
+            <div className="flex items-center justify-left text-xs gap-1.25 text-black">
+                <span
+                    key={project.owner.id}
+                    title={project.owner.name}
+                    className="flex items-center justify-center w-7 h-7 rounded-full bg-(--light-orange) text-[10px]"
+                >
+                    {getInitials(project.owner.name)}
+                </span>
+                <span className="text-sm bg-(--light-orange) text-(--dark-orange) px-4 py-1 rounded-full">Propriétaire</span>
                 <div className="flex -space-x-2">
                     {project.members.map((member) => (
                         <span
                             key={member.id}
                             title={member.user.name}
-                            className="flex items-center justify-center w-8 h-8 rounded-full bg-(--light-orange) border-2 border-white text-[10px]"
+                            className="flex items-center justify-center w-7 h-7 rounded-full bg-(--form-grey) border border-white text-[10px]"
                         >
-                            {member.user.name.slice(0, 2).toUpperCase()}
+                            {getInitials(member.user.name)}
                         </span>
                     ))}
                 </div>
